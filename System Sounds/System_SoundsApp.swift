702d04a6c29ae209408f7de384498c76dd549d23
Rishi <rishi_j@icloud.com>
2025-02-27 09:21:45 +0000

    Initial Commit

---
+++ System Sounds/System_SoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  System_SoundsApp.swift
+//  System Sounds
+//
+//  Created by Rishi Jansari on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct System_SoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
